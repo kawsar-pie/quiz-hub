c9cb0d8415c6d56b6079f3fb99dd41fb7a0d7516
kawsar-pie <79654190+kawsar-pie@users.noreply.github.com>
2023-05-13 19:31:27 +0600

    reload error fixed

--- src/components/CheckAnswer/CheckAnswer.jsx
+++ src/components/CheckAnswer/CheckAnswer.jsx
@@ -4,13 +4,13 @@ import './CheckAnswer.css'
 import 'react-toastify/dist/ReactToastify.css';
 const CheckAnswer = ({ answer, selectedAnswer }) => {
     let notify = () => toast.success("Correct");
-    if (selectedAnswer === "") {
+    if (selectedAnswer === undefined) {
         notify = () => toast.info("Please Select One!");
     }
     else if (answer !== selectedAnswer) {
         notify = () => toast.error("Wrong");
     }
-
+    // console.log(answer, selectedAnswer);
     return (
         <div onClick={notify}>
             <p className='check-answer'>Check Answer</p>
